4ebad86c7fae9910496a0dafc5129ecec74a391e
JoPa <johnpaulgitata@gmail.com>
2018-05-08 14:50:39 +0300

    finished the To-Do-List.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//business logic
+function Dolist(first){
+  this.firstList = first;
+}
+//user interface
+$(document).ready(function(){
+  $("form#list").submit(function (event) {
+    event.preventDefault();
+
+    var inputtedFirstList = $("input#new-list-name").val();
+
+    var newDolist = new Dolist(inputtedFirstList);
+
+    $("ol#tolist").append("<li>" + newDolist.firstList + "<button class='rem'>X </button>" + "</li>")
+
+    $('.rem').on("click", function(){
+      $(this).parent().remove();
+    })
+
+    $("input#new-list-name").val();
+  });
+});
